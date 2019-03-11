d0db071a821d390310935f97af297e2a23b2abb0
unknown <filipasenra4@gmail.com>
2019-03-11 15:14:09 +0000

    Progress

--- CGRA_3/MyUnitCube.js
+++ CGRA_3/MyUnitCube.js
@@ -24,8 +24,32 @@ class MyUnitCube extends CGFobject {
             0.5, -0.5, 0.5,	//5
             -0.5, -0.5, 0.5,	//6
             -0.5, 0.5, 0.5,		//7
-        ];
+            
+            // 3 plano
+            0.5, 0.5, -0.5,	//0
+            0.5, -0.5, -0.5,	//1
+            0.5, 0.5, 0.5,	//4
+            0.5, -0.5, 0.5,	//5
 
+            //4 plano
+            -0.5, 0.5, -0.5,	//3
+            -0.5, -0.5, -0.5,	//2
+            -0.5, -0.5, 0.5,	//6
+            -0.5, 0.5, 0.5,		//7
+
+            //5 plano
+            0.5, 0.5, 0.5,	//4
+            0.5, 0.5, -0.5,	//0
+            -0.5, 0.5, -0.5,	//3
+            -0.5, 0.5, 0.5,		//7
+
+            //6 plano
+            0.5, -0.5, -0.5,	//1
+            -0.5, -0.5, -0.5,	//2
+            0.5, -0.5, 0.5,	//5
+            -0.5, -0.5, 0.5,	//6
+        ];
+        
         //Counter-clockwise reference of vertices
         this.indices = [
             //quadrado eixo azul parte negativa
@@ -37,34 +61,61 @@ class MyUnitCube extends CGFobject {
             4, 7, 6,
 
             //quadrado eixo vermelho positivo
-            5, 1, 0,
-            0, 4,5,
+            11, 9, 8,
+            8, 10,11,
 
             //quadrado eixo vermelho negativo
-            3, 2, 6,
-            6, 7, 3,
+            12, 13, 14,
+            14, 15, 12,
 
             //quadrado eixo verde positivo
-            4, 0, 3,
-            3, 7, 4, 
+            16, 17, 18,
+            18, 19, 16, 
 
             //quadrado eixo verde negativo
-            2, 1, 5,
-            5, 6, 2
+            21, 20, 22,
+            22, 23, 21
             
         ];
 
         //vetores normais
         this.normals = [];
 
-        this.normals.push(0,1, 0);
-        this.normals.push(0,-1, 0);
-        this.normals.push(0,-1, 0);
-        this.normals.push(0,1, 0);
-        this.normals.push(0,1, 0);
-        this.normals.push(0,-1, 0);
-        this.normals.push(0,-1, 0);
-        this.normals.push(0,1, 0);
+        //1 plano
+        this.normals.push(0,0, -1);
+        this.normals.push(0,0, -1);
+        this.normals.push(0,0, -1);
+        this.normals.push(0,0, -1);
+        
+        //2 plano
+        this.normals.push(0, 0, 1);
+        this.normals.push(0,0, 1);
+        this.normals.push(0,0, 1);
+        this.normals.push(0,0, 1);
+
+        //3 plano
+        this.normals.push(1, 0, 0);
+        this.normals.push(1, 0, 0);
+        this.normals.push(1, 0, 0);
+        this.normals.push(1, 0, 0);
+
+        //4 plano
+        this.normals.push(-1, 0, 0);
+        this.normals.push(-1, 0, 0);
+        this.normals.push(-1, 0, 0);
+        this.normals.push(-1, 0, 0);
+
+        //5 plano
+        this.normals.push(0, 1, 0);
+        this.normals.push(0, 1, 0);
+        this.normals.push(0, 1, 0);
+        this.normals.push(0, 1, 0);
+
+        //6 plano
+        this.normals.push(0, -1, 0);
+        this.normals.push(0, -1, 0);
+        this.normals.push(0, -1, 0);
+        this.normals.push(0, -1, 0);
 
         this.primitiveType = this.scene.gl.TRIANGLES;
         this.initGLBuffers();
